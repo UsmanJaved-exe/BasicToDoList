de65e55b4396555665889fa5cafb9a210b00e545
UsmanJaved-exe <199630645+UsmanJaved-exe@users.noreply.github.com>
2026-04-26 17:02:21 +0500

    Made a simple to do list using lazy column and simple state using remember and remember saveable

---
+++ app/src/main/java/com/example/basictodolist/Task.kt
@@ -0,0 +1,90 @@
+package com.example.basictodolist
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun List() {
+    var task: String by rememberSaveable { (mutableStateOf("")) }
+    val listOfTasks = remember { (mutableStateListOf<String>()) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black)
+            .padding(20.dp)
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier,
+                value = task,
+                onValueChange = { task = it },
+                placeholder = { Text(text = "Write here...", fontSize = 20.sp, color = Color.Gray) }
+            )
+
+            Spacer(Modifier.weight(1f))
+
+            IconButton(
+                onClick = { listOfTasks.add(task); task = "" },
+                enabled = task.isNotEmpty()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AddCircle,
+                    tint = Color.LightGray,
+                    contentDescription = "Add task",
+                    modifier = Modifier.size(45.dp)
+                )
+            }
+        }
+
+        Spacer(Modifier.height(20.dp))
+
+        LazyColumn(modifier = Modifier, userScrollEnabled = true) {
+            items(listOfTasks) { listOfTasks ->
+                Text(text = listOfTasks, fontSize = 20.sp, color = Color.White)
+                Spacer(Modifier.height(10.dp))
+                HorizontalDivider()
+                Spacer(Modifier.height(10.dp))
+
+            }
+        }
+
+    }
+
+
+}
