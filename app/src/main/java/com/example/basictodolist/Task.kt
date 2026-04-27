9ea56ea87b05a04ea157b72039ed24e860f98f86
UsmanJaved-exe <199630645+UsmanJaved-exe@users.noreply.github.com>
2026-04-27 15:20:47 +0500

    Added Snackbar with Undo Functionality successfully.

--- app/src/main/java/com/example/basictodolist/Task.kt
+++ app/src/main/java/com/example/basictodolist/Task.kt
@@ -1,9 +1,6 @@
 package com.example.basictodolist
 
-import android.R.attr.content
-import android.os.ProfilingTrigger
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,15 +18,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalBottomSheetDefaults.properties
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -42,7 +40,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.DialogProperties
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -51,7 +48,13 @@ fun List() {
     val listOfTasks = remember { (mutableStateListOf<String>()) }
     //Alert Dialog state
     var trigger by rememberSaveable { (mutableStateOf(false)) }
+    //storing state of task to delete using alert dialog because state hoisting is not used
     var taskToDelete by rememberSaveable { (mutableStateOf("")) }
+    //storing the state of deleted task so it can be reverted via Snackbar
+    var deletedTask by rememberSaveable { (mutableStateOf("")) }
+    //SnackBar state
+    var showSnack by rememberSaveable { (mutableStateOf(false)) }
+
 
 
     Column(
@@ -63,11 +66,14 @@ fun List() {
         Row(
             Modifier
                 .fillMaxWidth()
-                .wrapContentHeight(),
+                .wrapContentHeight()
+                .padding(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             OutlinedTextField(
-                modifier = Modifier,
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .width(270.dp),
                 value = task,
                 onValueChange = { task = it },
                 placeholder = {
@@ -108,10 +114,17 @@ fun List() {
                 items(listOfTasks) { taskItems ->
 
                     Row(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Text(text = taskItems, fontSize = 20.sp, color = Color.White)
+                        Text(
+                            text = taskItems,
+                            fontSize = 20.sp,
+                            color = Color.White,
+                            modifier = Modifier
+                                .wrapContentHeight()
+                                .width(270.dp)
+                        )
                         Spacer(Modifier.weight(1f))
                         IconButton(onClick = {
                             trigger = true
@@ -135,14 +148,17 @@ fun List() {
     }
     if (trigger) {
         AlertDialog(
-            onDismissRequest = { trigger = false },
+            onDismissRequest = { },
             title = { Text(text = "Delete task") },
             text = { Text(text = "Are you sure you want to delete this task?") },
             confirmButton = {
                 Button(
                     onClick = {
+                        deletedTask = taskToDelete
                         listOfTasks.remove(taskToDelete)
+
                         trigger = false
+                        showSnack = true
                     },
                     colors = ButtonDefaults.buttonColors(Color.Red)
                 ) {
@@ -151,7 +167,7 @@ fun List() {
             },
             dismissButton = {
                 Button(
-                    onClick = { trigger = false },
+                    onClick = { },
                     colors = ButtonDefaults.buttonColors(Color.Blue)
                 ) {
                     Text(text = "No", fontSize = 15.sp, color = Color.White)
@@ -159,6 +175,23 @@ fun List() {
             },
             shape = RoundedCornerShape(15.dp)
         )
+    }
+
+    if (showSnack) {
+        Snackbar(
+            modifier = Modifier.padding(4.dp),
+            action = {
+                TextButton(onClick = {
+                    listOfTasks.add(deletedTask)
+                    showSnack = false
+                }) {
+                    Text(text = "UNDO", fontSize = 15.sp)
+                }
+            },
+            dismissAction = {},
+        ) {
+            Text(text = "Task Deleted", fontSize = 15.sp)
+        }
     }
 }
 
